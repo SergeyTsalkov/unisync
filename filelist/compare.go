ba6f4d9dd5fd9fd7e616dcf3e4d81ee3bfeef7a1
Sergey Tsalkov <stsalkov@gmail.com>
2022-09-21 18:31:31 -0500

    local/remote mkdir parts of syncplan are now cleaned of implied actions: 1. if a file is going to be put into a directory, then explicitly creating that directory is redundant 2. if we're going to create dir/subdir, then creating dir/ first is redundant

--- filelist/compare.go
+++ filelist/compare.go
@@ -1,6 +1,10 @@
 package filelist
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+)
 
 type SyncPlan struct {
 	Pull        []*FileListItem
@@ -61,7 +65,53 @@ func Compare(_localList, _remoteList FileList) *SyncPlan {
 		}
 	}
 
+	plan.LocalMkdir = clean(plan.LocalMkdir, plan.LocalMkdir)
+	plan.LocalMkdir = clean(plan.LocalMkdir, plan.Pull)
+
+	plan.RemoteMkdir = clean(plan.RemoteMkdir, plan.RemoteMkdir)
+	plan.RemoteMkdir = clean(plan.RemoteMkdir, plan.Push)
+
 	return plan
+}
+
+func clean(list, list2 []*FileListItem) []*FileListItem {
+	cleanlist := []*FileListItem{}
+
+	for _, item := range list {
+		isClean := true
+
+		for _, item2 := range list2 {
+			if HasPrefix(item2.Path, item.Path) {
+				isClean = false
+				break
+			}
+		}
+
+		if isClean {
+			cleanlist = append(cleanlist, item)
+		}
+	}
+
+	return cleanlist
+}
+
+func HasPrefix(_long, _short string) bool {
+	long := strings.Split(_long, string(filepath.Separator))
+	short := strings.Split(_short, string(filepath.Separator))
+
+	if len(long) <= len(short) {
+		return false
+	}
+
+	for len(long) > 0 && len(short) > 0 {
+		if long[0] != short[0] {
+			return false
+		}
+		long = long[1:]
+		short = short[1:]
+	}
+
+	return true
 }
 
 func (p *SyncPlan) Show() {
